b8a51992cba312bb5ca1b6b0d01d1e926b63c368
logixode <rohmad1230@gmail.com>
2025-08-04 11:31:34 +0700

    feat: create input group component

---
+++ src/components/ui/input/InputGroup.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="input-group flex items-center w-full relative">
+    <div
+      class="absolute inset-y-0 flex items-center justify-items-center px-2"
+      v-if="!!slots['left']"
+    >
+      <slot name="left" />
+    </div>
+    <div class="flex-1 min-w-0">
+      <slot />
+    </div>
+
+    <div
+      class="absolute end-0 inset-y-0 flex items-center justify-items-center px-2"
+      v-if="!!slots['right']"
+    >
+      <slot name="right" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useSlots } from 'vue'
+
+const slots = useSlots()
+</script>
